a706f2e1efa0598dbc68b8736e26b2a6b98299c9
Vedant Chainani <vedantchainani1084@gmail.com>
2023-09-11 22:26:40 +0530

    add game results components

--- app/components/game/index.ts
+++ app/components/game/index.ts
@@ -1,4 +1,15 @@
 import CandlestickChart from './candlestick-chart';
 import PlaceBet from './place-bet';
+import LatestBetsTable from './latest-bets';
+import NoResults from './no-results';
+import BestLost from './best-lost';
+import ClaimRewards from './claim-rewards';
 
-export { CandlestickChart, PlaceBet };
+export {
+	CandlestickChart,
+	PlaceBet,
+	LatestBetsTable,
+	NoResults,
+	BestLost,
+	ClaimRewards,
+};
--- app/sections/game/index.tsx
+++ app/sections/game/index.tsx
@@ -1,21 +1,35 @@
-import { CandlestickChart, PlaceBet } from '@/components/game';
 import React from 'react';
+import {
+	CandlestickChart,
+	PlaceBet,
+	LatestBetsTable,
+	NoResults,
+	BestLost,
+	ClaimRewards,
+} from '@/components/game';
 
 interface GameProps {
 	id: string;
 }
 
 const Game = ({ id }: GameProps) => {
+	let gameEnded = true;
+	let resultsDeclared = true;
+	let won = true;
 	return (
 		<div className='flex flex-col gap-8 p-4 py-8 '>
 			<div className='flex flex-col gap-8 lg:flex-row'>
 				<div className='order-2 w-full basis-1/3 lg:order-1'>
-					<PlaceBet />
+					{!gameEnded && <PlaceBet />}
+					{gameEnded && !resultsDeclared && <NoResults />}
+					{gameEnded && resultsDeclared && won && <ClaimRewards />}
+					{gameEnded && resultsDeclared && !won && <BestLost />}
 				</div>
 				<div className='order-1 w-full basis-2/3 lg:order-2'>
 					<CandlestickChart />
 				</div>
 			</div>
+			<LatestBetsTable />
 		</div>
 	);
 };
