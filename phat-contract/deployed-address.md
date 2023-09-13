fdb1a02c09d4d18716f44af88180735fd0b9c157
Vedant Chainani <vedantchainani1084@gmail.com>
2023-09-14 02:07:39 +0530

    update mainnet scripts

--- phat-contract/deployed-address.md
+++ phat-contract/deployed-address.md
@@ -10,5 +10,5 @@ Mainnet -
 
 ## Phat function
 
-Po5 -
-Mainnet -
+Po5 - https://bricks-poc5.phala.network/workflows/0x1d75072ca200208088d1c51e69c9f3e3b1ff125e7ff9b716624f10ee38908c76/0
+Mainnet - https://bricks.phala.network/workflows/0xbf840aa11319bddc377ce2b859daa968cb847e4ec53f94e97c33bca57997fcd8/0
--- phat-contract/scripts/polygon/deploy.ts
+++ phat-contract/scripts/polygon/deploy.ts
@@ -1,33 +1,36 @@
-import { ethers } from "hardhat";
-import "dotenv/config";
-import dedent from "dedent";
+import { ethers } from 'hardhat';
+import 'dotenv/config';
+import dedent from 'dedent';
 
 async function main() {
-  const TestLensApiConsumerContract = await ethers.getContractFactory("TestLensApiConsumerContract");
+	const SkybetTokenContract = await ethers.getContractFactory('SkybetToken');
+	const SkybetContract = await ethers.getContractFactory('Skybet');
 
-  const [deployer] = await ethers.getSigners();
+	const [deployer] = await ethers.getSigners();
 
-  console.log('Deploying...');
-  const attestor = process.env['POLYGON_LENSAPI_ORACLE_ENDPOINT'] || deployer.address;  // When deploy for real e2e test, change it to the real attestor wallet.
-  const consumer = await TestLensApiConsumerContract.deploy(attestor);
-  await consumer.deployed();
-  const finalMessage = dedent`
-    🎉 Your Consumer Contract has been deployed, check it out here: https://polygonscan.com/address/${consumer.address}
+	console.log('Deploying Skybet Token...');
+	const token = await SkybetTokenContract.deploy();
+	await token.deployed();
+	console.log(`Skybet Token deployed to: ${token.address}`);
+
+	console.log('Deploying Skybet...');
+	const skybet = await SkybetContract.deploy(token.address);
+	await skybet.deployed();
+	console.log(`Skybet deployed to: ${skybet.address}`);
+
+	const finalMessage = dedent`
+    🎉 Your Skybet  Contract has been deployed, check it out here: https://polygonscan.com/address/${skybet.address}
     
     You also need to set up the consumer contract address in your .env file:
     
-    POLYGON_CONSUMER_CONTRACT_ADDRESS=${consumer.address}
-  `
-  console.log(`\n${finalMessage}\n`);
-
-  console.log('Sending a request...');
-  await consumer.connect(deployer).request("0x01c567");
-  console.log('Done');
+    POLYGON_CONSUMER_CONTRACT_ADDRESS=${skybet.address}
+  `;
+	console.log(`\n${finalMessage}\n`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
